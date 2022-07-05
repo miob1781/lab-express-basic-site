55e1cf4eb18da90a381ae281c775125b73f9c9ea
jackletran <jack.letran@moebel.de>
2022-07-05 17:34:59 +0200

    fix backend

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-    
-})
+  response.sendFile(__dirname + "/views/home.html");
+});
 
-app.listen(3000, ()=> {
-    console.log("App is listening on PORT 3000...")
-})
+app.get("/about", (request, response) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/photos", (request, response) => {
+  response.sendFile(__dirname + "/views/photos.html");
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on PORT 3000...");
+});
